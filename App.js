93d4a4b2dd659106fec4d9cbc8d1dc642d53fa22
Gabriel Genovez <86001324+GabrielG9z@users.noreply.github.com>
2023-01-23 11:55:22 -0300

    Update
    Nessa branch foram feitos os componentes de exibição das seguintes funções programadas 'capturar foto' 'exibir localização'

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import React, { useState, useEffect } from "react";
-import { Button, Image, Text, SafeAreaView, View } from "react-native";
+import {
+  Button,
+  Image,
+  Text,
+  SafeAreaView,
+  StatusBar,
+  View,
+  TextInput,
+  StyleSheet,
+} from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import * as Location from "expo-location";
 import MapView, { Marker } from "react-native-maps";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export default function ImagePickerExample() {
   const [status, requestPermission] = ImagePicker.useCameraPermissions();
   const [foto, setFoto] = useState();
+  const [titulo, onChangeText] = useState();
 
-  useEffect(() => {
+  const obterTitulo = useEffect(() => {
     async function verificaPermissoes() {
       const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
       requestPermission(cameraStatus === "granted");
     }
-
     verificaPermissoes();
   }, []);
 
@@ -27,82 +37,110 @@ export default function ImagePickerExample() {
     setFoto(imagem.assets[0].uri);
   };
 
+  const [minhaLocalizacao, setMinhaLocalizacao] = useState(null);
+
+  useEffect(() => {
+    async function obterLocalizacao() {
+      const { status } = Location.requestForegroundPermissionsAsync();
+
+      let localizacaoAtual = await Location.getCurrentPositionAsync({});
+
+      setMinhaLocalizacao(localizacaoAtual);
+    }
+    obterLocalizacao();
+  }, []);
+  console.log(minhaLocalizacao);
+  const regiaoInicial = {
+    latitude: -23.533773,
+    longitude: -46.65529,
+    latitudeDelta: 10,
+    longitudeDelta: 10,
+  };
+
+  const [localizacao, setLocalizacao] = useState();
+
+  const marcarLocal = (event) => {
+    setLocalizacao({
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+      latitude: minhaLocalizacao.coords.latitude,
+      longitude: minhaLocalizacao.coords.longitude,
+    });
+    console.log(localizacao);
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button title="Acessar câmera" onPress={acessarCamera} />
-      {foto && (
-        <Image source={{ uri: foto }} style={{ width: 300, height: 200 }} />
-      )}
-    </View>
-  );
-
-  function App() {
-    /* State para a geolocalização */
-    const [minhaLocalizacao, setMinhaLocalizacao] = useState(null);
-
-    const [status, requestPermission] = Location.useForegroundPermissions();
-
-    useEffect(() => {
-      async function verificaPermissoes() {
-        const { locationStatus } =
-          await Location.requestForegroundPermissionsAsync();
-        requestPermission(locationStatus === "granted");
-
-        let localizacaoAtual = await Location.getCurrentPositionAsync({});
-        console.log("Status: " + status.status);
-        setMinhaLocalizacao(localizacaoAtual);
-      }
-
-      verificaPermissoes();
-    }, []);
-
-    console.log(minhaLocalizacao);
-
-    const regiaoInicial = {
-      // Estado de SP
-      latitude: -23.533773,
-      longitude: -46.65529,
-      latitudeDelta: 10,
-      longitudeDelta: 10,
-    };
-
-    /* Usando state para controlar a localização */
-    const [localizacao, setLocalizacao] = useState();
-
-    const marcarLocal = (event) => {
-      setLocalizacao({
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-        latitude: minhaLocalizacao.coords.latitude,
-        longitude: minhaLocalizacao.coords.longitude,
-      });
-    };
-
-    return (
-      <>
-        <StatusBar />
-        <View style={estilos.container}>
-          <View style={estilos.viewBotao}>
-            <Button title="Onde estou?" onPress={marcarLocal} />
-          </View>
-          <View style={estilos.viewMapa}>
-            <MapView
-              style={estilos.mapa}
-              region={localizacao ?? regiaoInicial}
-              liteMode={false}
-              mapType="standard"
-            >
-              {localizacao && (
-                <Marker
-                  coordinate={localizacao}
-                  title="Aqui!!!"
-                  onPress={(e) => console.log(e.nativeEvent)}
-                />
-              )}
-            </MapView>
-          </View>
+    <>
+      <StatusBar />
+      <SafeAreaView
+        style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+      >
+        <View style={estilos.titulo}>
+          <TextInput
+            style={estilos.input}
+            onChangeText={onChangeText}
+            value={titulo}
+            placeholder="Digite um título"
+            textAlign="center"
+          />
         </View>
-      </>
-    );
-  }
+        {foto && (
+          <Image source={{ uri: foto }} style={{ width: 300, height: 200 }} />
+        )}
+        <View style={estilos.botao}>
+          <Button title="Take a Photo" onPress={acessarCamera} color="black" />
+        </View>
+
+        <MapView
+          onPress={marcarLocal}
+          style={estilos.mapa}
+          region={localizacao ?? regiaoInicial}
+          liteMode={false}
+          mapType="hybrid"
+        >
+          {localizacao && (
+            <Marker
+              coordinate={localizacao}
+              title="Aqui !"
+              onPress={(e) => console.log(e.nativeEvent)}
+            />
+          )}
+        </MapView>
+        <View style={estilos.botao}>
+          <Button
+            title="Find my Location"
+            onPress={marcarLocal}
+            color="black"
+          />
+        </View>
+      </SafeAreaView>
+    </>
+  );
 }
+
+const estilos = StyleSheet.create({
+  botao: {
+    margin: 8,
+    borderWidth: "2px",
+    borderRadius: "3px",
+    backgroundColor: "#63F893",
+  },
+  mapa: {
+    width: 300,
+    height: 250,
+  },
+  container: {
+    flex: 1,
+  },
+
+  titulo: {
+    margin: 5,
+    borderWidth: "2px",
+    borderRadius: "4px",
+    width: 300,
+    borderColor: "#63BAF8",
+  },
+  input: {
+    fontSize: 22,
+  },
+});
